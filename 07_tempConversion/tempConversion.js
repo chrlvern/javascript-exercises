19261bdb5667fe29a8b272233595dcb6ad972f1d
Charley Vernon <131111628+chrlvern@users.noreply.github.com>
2023-04-30 16:55:49 -0600

    Complete JS Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convert = (temp - 32)*(5/9);
+  if(convert % 1 != 0){
+    return Math.round(convert*10)/10;
+  }
+  else{
+    return convert;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convert = (temp * (9/5)) + 32;
+  if(convert % 1 != 0){
+    return Math.round(convert*10)/10;
+  }
+  else{
+    return convert;
+  }
 };
 
 // Do not edit below this line
