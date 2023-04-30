a346153c96dceee245eb6b29e73965293f759e40
Charley Vernon <131111628+chrlvern@users.noreply.github.com>
2023-04-30 16:03:03 -0600

    Complete JS Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
 
+    for(let i = 1; i < arguments.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if(arguments[i] == arr[j]){
+                if(typeof arguments[i] == typeof arr[j]){
+                    arr.splice(j, 1);
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
